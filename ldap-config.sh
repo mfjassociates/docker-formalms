dc8180cf56d2d6ec715f1be845548c5b768fd497
Mario Jauvin <marioja@users.noreply.github.com>
2025-03-01 16:37:44 -0500

    Use UI ldap values

--- ldap-config.sh
+++ ldap-config.sh
@@ -8,7 +8,6 @@ set -e
 set -u
 
 # Read the content of the secret file into a variable
-pw=$(cat /run/secrets/ldap_bind_pw)
 db_pw=$(cat /run/secrets/formalms_db_pw)
 
 # Configure database settings
@@ -18,20 +17,15 @@ sed -i "s|cfg\\['db_pass'\\] = ''|cfg['db_pass'] = '${db_pw}'|" /app/formalms/co
 sed -i "s|cfg\\['db_name'\\] = 'forma'|cfg['db_name'] = '${DB_NAME}'|" /app/formalms/config.php
 
 # Check if LDAP settings already exist
-if ! grep -q "cfg\['user_pwd_type'\]" /app/formalms/config.dist.php; then
+if ! grep -q "cfg\['ldap_server'\]" /app/formalms/config.php; then
   echo "
   // LDAP Configuration
-  \$cfg['user_pwd_type'] = 'ldap';
-  \$cfg['ldap_host'] = '${LDAP_HOST}';
+  \$cfg['ldap_server'] = '${LDAP_HOST}';
   \$cfg['ldap_port'] = '${LDAP_PORT}';
-  \$cfg['ldap_base_dn'] = '${LDAP_BASE_DN}';
-  \$cfg['ldap_bind_dn'] = '${LDAP_BIND_DN}';
-  \$cfg['ldap_bind_password'] = '${pw}';
-  \$cfg['ldap_filter'] = '${LDAP_USER_FILTER}';
-  \$cfg['ldap_user_attr'] = '${LDAP_USERNAME_ATTRIBUTE}';
-  \$cfg['ldap_realname_attr'] = '${LDAP_REALNAME_ATTRIBUTE}';
-  \$cfg['ldap_email_attr'] = '${LDAP_EMAIL_ATTRIBUTE}';
-  " >> /app/formalms/config.dist.php
+  \$cfg['ldap_user_string'] = '${LDAP_USER_STRING}';
+  \$cfg['ldap_alternate_check'] = '${LDAP_ALTERNATE_CHECK}';
+  \$cfg['ldap_used'] = '${LDAP_USERED}';
+  " >> /app/formalms/config.php
   echo "LDAP settings added to config.php"
 else
   echo "LDAP settings already exist in config.php"
